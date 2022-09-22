108023790611ad74b45093d12a1381f1387b5d83
MD Mobin <74850678+Djsmk123@users.noreply.github.com>
2022-09-23 00:06:57 +0530

    initial commit

---
+++ app/src/main/java/com/smkwinner/trivia2/components/score_card.kt
@@ -0,0 +1,14 @@
+package com.smkwinner.trivia2.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun ScoreCard(title:String,subtitle:String){
+    Column() {
+        TextWidget(txt = title, clr = Color.Black, fontSize = 16.0)
+        TextWidget(txt = subtitle, clr = Color.Black, fontSize = 32.0,weight = FontWeight.SemiBold)
+    }
+}
